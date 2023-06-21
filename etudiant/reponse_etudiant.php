225016de00d366078c6c8d5c2f4c3e8936832258
Ayman <22014@supnum.mr>
2023-06-21 11:57:39 +0000

    modifier par ayman

--- etudiant/reponse_etudiant.php
+++ etudiant/reponse_etudiant.php
@@ -10,33 +10,6 @@ include_once "../connexion.php";
 
 $id_sous = $_GET['id_sous'];
 
-function test_input($data){
-    $data = htmlspecialchars($data);
-    $data = trim($data);
-    $data = htmlentities($data);
-    $data = stripcslashes($data);
-    return $data;
-}
-if(isset($_POST['button'])){
-        $descri=test_input($_POST['description_sous']);
-        $file= $_POST['file'];
-
-    if( !empty($descri) or !empty($file) ){
-        $sql="INSERT INTO `reponses`(`description_rep`,`data_reponse`, `id_sous`, `id_etud`) VALUES('$descri','$file','$id_sous',(select id_etud from etudiant where email = '$email')) ";
-
-        $req = mysqli_query($conn,$sql);
-        if($req){
-            header("location: etudiant.php");
-        }else {
-            $message = "Etudiant non ajouté";
-        }
-
-    }else {
-       $message = "Veuillez remplir tous les champs !";
-    }
-}
-include "../nav_bar.php";
-
 ?>
 <script type="text/JavaScript">
     var i = 1;
@@ -77,14 +50,47 @@ include "../nav_bar.php";
             
             <ol class="breadcrumb">
             <li><a href="#">Acceuil</a></li>
-            <li>Gestion des soumission</li>
+            <li>Rendre  le travail</li>
             </ol>
         </div>
     </div>
    
 <div class="form-horizontal">
     <br /><br />
+<?php
+    $sql = "select * from reponses where id_sous = '$id_sous' and id_etud = (select id_etud from etudiant where email = '$email') ";
+$req = mysqli_query($conn,$sql);
 
+if (mysqli_num_rows($req) == 0) {
+
+function test_input($data){
+    $data = htmlspecialchars($data);
+    $data = trim($data);
+    $data = htmlentities($data);
+    $data = stripcslashes($data);
+    return $data;
+}
+if(isset($_POST['button'])){
+        $descri=test_input($_POST['description_sous']);
+        $file= $_POST['file'];
+
+    if( !empty($descri) or !empty($file) ){
+        $sql="INSERT INTO `reponses`(`description_rep`,`data_reponse`, `id_sous`, `id_etud`) VALUES('$descri','$file','$id_sous',(select id_etud from etudiant where email = '$email')) ";
+
+        $req = mysqli_query($conn,$sql);
+        if($req){
+            header("location: etudiant.php");
+        }else {
+            $message = "Etudiant non ajouté";
+        }
+
+    }else {
+       $message = "Veuillez remplir tous les champs !";
+    }
+}
+include "../nav_bar.php";
+
+?>
     <p class="erreur_message">
             <?php 
             if(isset($message)){
@@ -118,11 +124,81 @@ include "../nav_bar.php";
         <div class="form-group">
             <div class="col-md-offset-2 col-md-10">
                 <input type="submit" name="button" value=Enregistrer class="btn-primary"  />
+            </div>
+        </div>
+</form>
+
+<?php
+}else{
+    function test_input($data){
+        $data = htmlspecialchars($data);
+        $data = trim($data);
+        $data = htmlentities($data);
+        $data = stripcslashes($data);
+        return $data;
+    }
+    if(isset($_POST['button'])){
+            $descri=test_input($_POST['description_sous']);
+            $file= $_POST['file'];
+    
+        if( !empty($descri) or !empty($file) ){
+            $sql="UPDATE reponses set description_rep = '$descri' ,  `date` = NOW() where id_sous = '$id_sous' and id_etud=(select id_etud from etudiant where email = '$email') ";
+    
+            $req = mysqli_query($conn,$sql);
+            if($req){
+                header("location: index_etudiant.php");
+            }else {
+                $message = "Reponse non ajouté";
+            }
+    
+        }else {
+           $message = "Veuillez remplir tous les champs !";
+        }
+    }
+    include "../nav_bar.php";
+    $sql = "SELECT * FROM reponses  WHERE  id_sous = '$id_sous' and id_etud = (select id_etud from etudiant where email = '$email')";
+    $req1 = mysqli_query($conn , $sql);
+    $row = mysqli_fetch_assoc($req1);
+      
+?>
+        <p class="erreur_message">
+            <?php 
+            if(isset($message)){
+                echo $message;
+            }
+            ?>
+        </p>
+        <form action="" method="POST">
+        <div class="form-group">
+            <label class="col-md-1">Description </label>
+            <div class="col-md-6">
+                <textarea name="description_sous" id="" class = "form-control" cols="30" rows="10" ><?=$row['description_rep']?></textarea>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-1">Sélectionnez un fichier : </label>
+            <div class="col-md-6">
+                <input type="file" id="fichier" name="file" class = "form-control">
+            </div>
+        </div>
+        <div  id="newElementId">
+        </div>
+        <br>  <br> <br>
+        <div class="col-md-12">
+        <button type="button" onclick="createNewElement();">
+                Ajouter un fichier
+        </button>
+        </div>
+        <br>  <br> <br>
+        <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <input type="submit" name="button" value=Enregistrer class="btn-primary"  />
 
             </div>
         </div>
-
- 
 </form>
+<?php
+}
+?>
 </body>
 </html>
